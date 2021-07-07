84efbdc6767205066d9554bd85f3e178a150f02d
SpodnyMacicka <61090896+SpodnyMacicka@users.noreply.github.com>
2021-07-07 15:09:27 +0300

    Add files via upload

---
+++ Циклы/Masez/EmptyMazeTask.cs
@@ -0,0 +1,26 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            MoveRight(robot, width);
+            MoveDown(robot, height);
+        }
+
+        public static void MoveRight(Robot robot, int count)
+        {
+            for(int i = 0; i < count -3;i++)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+        }
+        public static void MoveDown(Robot robot, int count)
+        {
+            for (int i = 0; i < count - 3; i++)
+            {
+                robot.MoveTo(Direction.Down);
+            }
+        }
+    }
+}
